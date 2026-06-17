ccd173fb43aaa288ab1a7a8b80fc4056d2237a81
Daaboulex <39669593+Daaboulex@users.noreply.github.com>
2026-06-17 14:31:16 +0200

    chore: adopt nix-packaging-standard v2.7.0 (ci.yml reclaims runner disk for large builds)

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     std = {
-      url = "github:Daaboulex/nix-packaging-standard?ref=v2.6.1";
+      url = "github:Daaboulex/nix-packaging-standard?ref=v2.7.0";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.git-hooks.follows = "git-hooks";
     };
